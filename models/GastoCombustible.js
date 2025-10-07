798743d3d54ced5579311ea4d872d14bb67651a8
Andoni <andlopez@birt.eus>
2025-10-07 20:51:51 +0200

    Primer Commit

--- models/GastoCombustible.js
+++
@@ -1,9 +0,0 @@
-export class GastoCombustible {
-  constructor(id, vehicleType, date, kilometers, precioViaje) {
-    this.id = id;
-    this.vehicleType = vehicleType;
-    this.date = new Date(date); 
-    this.kilometers = parseFloat(kilometers);
-    this.precioViaje = parseFloat(precioViaje);
-  }
-}
